42e7456619e999250e28f6b01196d91112dd12c7
Oksana Semko <osemko@gmail.com>
2022-07-26 16:10:28 +0300

    File compiled for render

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Profile } from './components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from './components/FriendList/FriendList';
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 import user from './data/user.json';
 import statisticData from './data/data.json';
 import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 
 
 
@@ -20,6 +22,7 @@ export const App = () => {
       <Statistics title='Upload stats' stats={statisticData}></Statistics>
       <Statistics stats={statisticData}></Statistics>
       <FriendList friends={friends}></FriendList>
+      <TransactionHistory items={transactions}></TransactionHistory>
     </div>
   )
 }
