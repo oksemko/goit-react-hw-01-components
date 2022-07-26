005b578e614f498ade778ab34f2601d6c519f6ae
Oksana Semko <osemko@gmail.com>
2022-07-26 14:45:42 +0300

    Task_solution

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import styles from './FriendList.module.css';
+import { FriendListItem } from './FriendListItem/FriendListItem';
+
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={styles.friendlist}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendListItem
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+          key={id}
+        />
+      ))}
+    </ul>
+  );
+}
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(PropTypes.object).isRequired,
+}
---
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import styles from '../FriendList.module.css';
+
+
+export const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <li className={styles.item}>
+      <span
+        className={styles.status}
+        style={{ backgroundColor: isOnline ? 'green' : 'red' }}
+      ></span>
+      <img
+        className={styles.avatar}
+        src={avatar}
+        alt={'User avatar ' + { name }}
+        width="48"
+      />
+      <p className={styles.name}>{name}</p>
+    </li>
+  );
+}
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+}
