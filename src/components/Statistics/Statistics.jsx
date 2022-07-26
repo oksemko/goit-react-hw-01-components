7cc1098f566543f263002bd21780d380438265cb
Oksana Semko <osemko@gmail.com>
2022-07-26 04:26:39 +0300

    Added Statictics component

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import styles from './Statistics.module.css';
+import { getRandomHexColor } from 'utils/getRandomHexColor';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={styles.statistics}>
+      {title && <h2 className={styles.title}>{title}</h2>}
+
+      <ul className={styles.stat-list}>
+        {stats.map(({ id, label, percentage }) => {
+          return (
+            <li
+              className={styles.item}
+              key={id}
+              style={{ backgroundColor: getRandomHexColor() }}
+            >
+              <span className={styles.label} >{label}</span>
+              <span className={styles.percentage}>{percentage}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+}
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    }),
+  ),
+}
